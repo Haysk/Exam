6c3c33b481d3a7f54d676b99110bb6ed8bd4a721
adylewsk <adylewsk@student.42.fr>
2022-04-12 16:12:21 +0200

    ok

--- 04/main.c
+++ 04/main.c
@@ -1,55 +1,85 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <string.h>
 
 typedef struct s_list {
 	char **args;
-	int	type;
-	int pipe[2];
-	struct s_list *next; 
-	struct s_list *prev; 
+	int	pipe[2];
+	struct	s_list *next;
+	struct	s_list *prev;
 } t_list;
 
-int	ft_strlen(char *str) {
+int ft_strlen(char *str) {
 	int i;
 
 	i = 0;
 	if (!str || !str[0])
-		return 0;
-	while (str[i])
+		return (0);
+	while (str[i]) {
 		i++;
-	return i;
+	}
+	return (i);
 }
 
 char *ft_strdup(char *str) {
-	char *cp;
 	int i;
+	char *cpy;
 
-	cp = malloc(sizeof(char) * (ft_strlen(str) + 1));
 	i = 0;
-	while (str[i])
-	{
-		cp[i] = str[i];
+	cpy = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	while (str[i]) {
+		cpy[i] = str[i];
 		i++;
 	}
-	cp[i] = 0;
-	return cp;
+	cpy[i] = 0;
+	return (cpy);
 }
 
-void	free_list(t_list *first) {
-	t_list *tmp;
+char **create_args(char **av, int len) {
+	char **args;
 	int i;
-	
+
+	i = 0;
+	args = malloc(sizeof(char*) * (len + 1));
+	while (i < len) {
+		args[i] = ft_strdup(av[i]);
+		i++;
+	}
+	args[i] = NULL;
+	return (args);
+}
+
+t_list *add_back(t_list *first, char **args) {
+	t_list *node;
+	t_list *tmp;
+
+	node = malloc(sizeof(t_list));
+	node->next = NULL;
+	node->prev = NULL;
+	node->args = args;
 	if (!first)
-		return;
-	while (first)
-	{
+		return (node);
+	tmp = first;
+	while(tmp) {
+		if (!tmp->next) {
+			tmp->next = node;
+			node->prev = tmp;
+			return (first);
+		}
+		tmp = tmp->next;
+	}
+	return first;
+}
+
+t_list	*free_list (t_list *first) {
+	int i;
+	t_list *tmp;
+	while (first) {
 		i = 0;
-		while (first->args[i])
-		{
+		while(first->args[i]) {
 			free(first->args[i]);
 			i++;
 		}
@@ -58,142 +88,84 @@ void	free_list(t_list *first) {
 		first = first->next;
 		free(tmp);
 	}
-	first = NULL;
+	return (NULL);
 }
 
-char**	create_cmd(char **av, int len) {
-	int i;
-	char **args;
-
-	i = 0;
-	args = malloc(sizeof(char*) * (len + 1));
-	while (i < len)
-	{
-		args[i] = ft_strdup(av[i]);
-		i++;
-	}
-	args[i] = 0;
-	return (args);
-}
-
-t_list*	add_list(t_list *first, char **args) {
-	t_list *node;
+void	exec_cmd(t_list *first, char **env) {
 	t_list *tmp;
+	int	pid;
 
-	node = malloc(sizeof(t_list));
-	node->args = args;
-	node->next = NULL;
-	node->prev = NULL;
-	node->type = 0;
-	if (args == NULL)
-		node->type = 1;
-	if (!first)
-	{
-		first = node;
-		return first;
-	}
-	tmp = first;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = node;
-	node->prev = tmp;
-	return first;
-}
-
-void	cd(t_list *cmd) {
-	int i;
-
-	i = 0;
-	while (cmd->args[i])
-		i++;
-	if (i != 2)
-		printf("error cd");
-	else
-		chdir(cmd->args[1]);
-}
-
-void	exec_cmd(char **env, t_list *cmd) {
-	(void)env;
-	execve(cmd->args[0], cmd->args, env);
-	printf("error: cannot execute %s\n", cmd->args[0]);
-	exit(0);
-}
-
-void	exec_cmds(char **env, t_list *first)
-{
-	t_list	*tmp;
-	int		pid;
-
-	tmp = first;
-	while (tmp)
-	{
-			pipe(tmp->pipe);
-		if (strcmp(tmp->args[0], "cd") == 0)
-			cd(tmp);
-		else {
+	if (!strcmp(first->args[0], "cd")) {
+		if (first->args[1] && !first->args[2])
+			chdir(first->args[1]);
+		else
+			printf("error\n");
+	} else {
+		tmp = first;
+		while (tmp) {
+			//if (tmp->next)
+				pipe(tmp->pipe);
 			pid = fork();
 			if (pid == 0) {
 				if (tmp->next && dup2(tmp->pipe[1], 1) < 0)
-					printf("error");
+					printf("error1\n");
 				if (tmp->prev && dup2(tmp->prev->pipe[0], 0) < 0)
-					printf("error");
-				exec_cmd(env, tmp);
+					printf("error2\n");
+				execve(tmp->args[0], tmp->args, env);
+				printf("error3\n");
+				exit(0);
 			} else {
 				waitpid(pid, NULL, 0);
 				close(tmp->pipe[1]);
-				if (!tmp->next)
-					close(tmp->pipe[0]);
 				if (tmp->prev)
 					close(tmp->prev->pipe[0]);
+				if (!tmp->next)
+					close(tmp->pipe[0]);
 			}
+			tmp = tmp->next;
 		}
-		tmp = tmp->next;
 	}
 }
 
-int	parse(char **av, char **env, t_list **first)
-{
+int	parse(char **av, char **env) {
 	int i;
-	int	j;
-	char **args;
+	int j;
+	t_list *first;
 
 	i = 0;
-	args = NULL;
-	while (av[i] && strcmp(av[i], ";") != 0)
+	first = NULL;
+	while (av[i] && strcmp(av[i], ";"))
 	{
 		j = 0;
-		while (av[i] && strcmp(av[i], "|") != 0 && strcmp(av[i], ";") != 0)
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 		{
 			i++;
 			j++;
 		}
-		args = create_cmd(av + (i - j), j);
-		*first = add_list(*first, args);
-		if (av[i] && strcmp(av[i], "|") == 0)
+		first = add_back(first, create_args(av + (i - j), j));
+		if (av[i] && strcmp(av[i], ";"))
 			i++;
 	}
-	exec_cmds(env, *first);
-	free_list(*first);
-	*first = NULL;
+	if (first) {
+		exec_cmd(first, env);
+		first = free_list(first);
+	}
 	return (i);
 }
 
 int main(int ac, char **av, char **env)
 {
 	int i;
-	t_list *first;
 
 	i = 0;
-	first = NULL;
-	(void)env;
 	if (ac < 2)
-		return (0);
+		return 0;
 	av++;
 	while (av[i])
 	{
-		i += parse(av + i, env, &first);
+		i += parse(av + i, env);
 		if (av[i])
-		   i++;
+			i++;
 	}
     return 0;
 }
